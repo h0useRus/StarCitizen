6b052bc2af7ed11fe71f8179a1d2c3e68fb4bc52
Defter <defter@bigmir.net>
2021-03-04 04:26:45 +0200

    Add more logs and checks to profile manager

--- SCTools/SCTools/Repository/ProfileManager.cs
+++ SCTools/SCTools/Repository/ProfileManager.cs
@@ -22,9 +22,11 @@ namespace NSW.StarCitizen.Tools.Repository
             _profiles.Clear();
             if (Directory.Exists(_profilesPath))
             {
+                _logger.Info($"Load profiles at path: {_profilesPath}");
                 var profileFiles = Directory.EnumerateFiles(_profilesPath, $"*{ProfileExtension}");
                 foreach (var profilePath in profileFiles)
                 {
+                    _logger.Info($"Load profile: {profilePath}");
                     var profileFile = new CfgFile(profilePath);
                     var profileData = profileFile.Read();
                     if (profileData != null)
@@ -40,31 +42,34 @@ namespace NSW.StarCitizen.Tools.Repository
 
         public bool CreateProfile(string profileName, CfgData profileData)
         {
+            _logger.Info($"Create profile: {profileName}");
             if (_profiles.ContainsKey(profileName))
             {
+                _logger.Warn($"New profile name: {profileName} already exist");
                 return false;
             }
-            if (!Directory.Exists(_profilesPath))
+            if (CreateProfilesDirectory())
             {
-                Directory.CreateDirectory(_profilesPath);
-            }
-            var profileFile = new CfgFile(GetProfilePath(profileName));
-            if (profileFile.Save(profileData))
-            {
-                _profiles.Add(profileName, profileData);
-                return true;
+                var profileFile = new CfgFile(GetProfilePath(profileName));
+                if (profileFile.Save(profileData))
+                {
+                    _profiles.Add(profileName, profileData);
+                    return true;
+                }
             }
             return false;
         }
 
         public bool SaveProfile(string profileName, CfgData profileData)
         {
-            if (_profiles.ContainsKey(profileName))
+            _logger.Info($"Save profile: {profileName}");
+            if (!_profiles.ContainsKey(profileName))
             {
-                if (!Directory.Exists(_profilesPath))
-                {
-                    Directory.CreateDirectory(_profilesPath);
-                }
+                _logger.Error($"Save profile: {profileName} not found");
+                return false;
+            }
+            if (CreateProfilesDirectory())
+            {
                 var profileFile = new CfgFile(GetProfilePath(profileName));
                 if (profileFile.Save(profileData))
                 {
@@ -77,12 +82,15 @@ namespace NSW.StarCitizen.Tools.Repository
 
         public bool RenameProfile(string oldProfileName, string newProfileName)
         {
+            _logger.Info($"Rename profile: {oldProfileName} to {newProfileName}");
             if (_profiles.ContainsKey(newProfileName))
             {
+                _logger.Warn($"New profile name: {newProfileName} already exist");
                 return false;
             }
             if (!_profiles.TryGetValue(oldProfileName, out var profileData))
             {
+                _logger.Error($"Rename profile: {oldProfileName} not found");
                 return false;
             }
             try
@@ -107,8 +115,30 @@ namespace NSW.StarCitizen.Tools.Repository
 
         public void DeleteProfile(string profileName)
         {
+            _logger.Info($"Delete profile: {profileName}");
             _profiles.Remove(profileName);
-            FileUtils.DeleteFileNoThrow(GetProfilePath(profileName));
+            if (!FileUtils.DeleteFileNoThrow(GetProfilePath(profileName)))
+            {
+                _logger.Error($"Delete profile failed: {profileName}");
+            }
+        }
+
+        private bool CreateProfilesDirectory()
+        {
+            if (!Directory.Exists(_profilesPath))
+            {
+                _logger.Info($"Create profiles directory: {_profilesPath}");
+                try
+                {
+                    Directory.CreateDirectory(_profilesPath);
+                }
+                catch (Exception e)
+                {
+                    _logger.Error(e, $"Failed create profiles directory: {_profilesPath}");
+                    return false;
+                }
+            }
+            return true;
         }
 
         private string GetProfilePath(string profileName) => Path.Combine(_profilesPath, profileName + ProfileExtension);
