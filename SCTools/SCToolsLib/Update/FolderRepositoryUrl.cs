9ac8206d8d3ab1edf5b2eaae9bffb786468ca1a5
Defter <defter@bigmir.net>
2021-04-25 18:36:35 +0300

    Better handle local repositories url (show relative repositories as full and translate full to relative)

---
+++ SCTools/SCToolsLib/Update/FolderRepositoryUrl.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+
+namespace NSW.StarCitizen.Tools.Lib.Update
+{
+    public static class FolderRepositoryUrl
+    {
+        public static string? Parse(string rootPath, string url)
+        {
+            if (Path.IsPathRooted(url))
+            {
+                string urlPath = Path.GetFullPath(url);
+                string rootDir = rootPath;
+                if (!rootDir.EndsWith(Path.DirectorySeparatorChar.ToString()))
+                {
+                    rootDir = rootDir + Path.DirectorySeparatorChar;
+                }
+                if (urlPath.StartsWith(rootDir, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    string relativePath = urlPath.Substring(rootDir.Length);
+                    return string.IsNullOrEmpty(relativePath) ? "." : relativePath;
+                }
+                return url;
+            }
+            if (Directory.Exists(Path.Combine(rootPath, url)))
+            {
+                return url;
+            }
+            return null;
+        }
+
+        public static string Build(string rootPath, string repository)
+            => Path.IsPathRooted(repository) ? repository : Path.Combine(rootPath, repository);
+    }
+}
