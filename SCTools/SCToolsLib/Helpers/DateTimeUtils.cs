ceb64b2842623872eef5f24731509c71e8fe529b
Defter <defter@bigmir.net>
2021-05-26 21:21:59 +0300

    Add write release timestamp into data folder after localization install completed

--- SCTools/SCToolsLib/Helpers/DateTimeUtils.cs
+++ SCTools/SCToolsLib/Helpers/DateTimeUtils.cs
@@ -7,5 +7,11 @@ namespace NSW.StarCitizen.Tools.Lib.Helpers
         private static readonly DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public static DateTime FromUnixTimeSeconds(long seconds) => _epoch.AddSeconds(seconds);
+
+        public static long ToUnixTimeSeconds(DateTime date)
+        {
+            var epoch = date - _epoch;
+            return (long)epoch.TotalSeconds;
+        }
     }
 }
